58c91885182778d41a0aa34564276273034322b0
AvalosRuben <rubenavalosmtz@gmail.com>
2026-08-19 10:54:44 -0600

    Initial files

---
+++ README.md
@@ -0,0 +1,9 @@
+# My portfolio
+
+This is the place where I show my projects and skills
+
+## Tech stack:
+
+- React
+- TypeScript
+- Tailwindcss
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-indigo-500 text-white text-8xl font-bold flex flex-col gap-4 items-center justify-center">
+      Hello World!
+      <p className="text-3xl">This is my portfolio</p>
+    </div>
+  );
+}
+
+export default App;
