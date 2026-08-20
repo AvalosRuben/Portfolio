2d38879c2a07b2803c631a69b2e38cb74aeac4be
AvalosRuben <rubenavalosmtz@gmail.com>
2026-08-19 21:24:51 -0600

    feat(TopBar): Created the top part o the top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-cream micro-grid text-white text-8xl font-bold flex flex-col gap-4 items-center justify-center">
+    <div className="w-full h-screen bg-cream micro-grid text-charcoal flex flex-col">
+      <header className="border-b border-charcoal bg-cream-dark sticky top-0 z-50 w-full">
+        <div className="border-charcoal border-b py-4 px-6 flex items-center justify-between text-xs text-charcoal-light font-mono">
+          <span>SYS.CLOCK :: 19.8.2026 // UTC-6</span>
+          <div className="flex gap-10 items-center">
+            <span>SYS.ONLINE</span>
+            <span>P(FILT) 45Pa</span>
+            <span>VFD 19%</span>
+            <span className="text-charcoal font-bold">CELLS • 19%</span>
+            <span>PORT......3</span>
+          </div>
+        </div>
+      </header>
       Hello World!
       <p className="text-3xl">This is my portfolio</p>
     </div>
