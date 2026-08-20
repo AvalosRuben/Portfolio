fcac1f009d94e0c608fffcc68609a7f68e2ab8eb
AvalosRuben <rubenavalosmtz@gmail.com>
2026-08-19 21:02:25 -0600

    feat(Background): Set the background to cream and added the micro grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-indigo-500 text-white text-8xl font-bold flex flex-col gap-4 items-center justify-center">
+    <div className="w-full h-screen bg-cream micro-grid text-white text-8xl font-bold flex flex-col gap-4 items-center justify-center">
       Hello World!
       <p className="text-3xl">This is my portfolio</p>
     </div>
