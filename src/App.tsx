6c365630eab2d8ac1ba507cdd8fbf4b6ef7ee218
AvalosRuben <rubenavalosmtz@gmail.com>
2026-08-20 08:13:19 -0600

    feat(Hero): Top decorations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-cream micro-grid text-charcoal flex flex-col">
+    <div className="w-full h-screen bg-cream micro-grid text-charcoal flex flex-col items-center">
       <header className="border-b border-charcoal/35 bg-cream-dark sticky top-0 z-50 w-full font-space tracking-wider">
         <div className="border-charcoal/10  border-b py-4 px-6 flex items-center justify-between text-xs text-charcoal-light ">
           <span>SYS.CLOCK :: 19.8.2026 // UTC-6</span>{" "}
@@ -20,7 +20,7 @@ function App() {
           </div>
           <div className="flex gap-10 items-center">
             {/*TODO - Change to nav and buttons to set the scroll to id*/}
-            <span className="font-space ">SUMMARY</span>
+            <span>SUMMARY</span>
             <span>EXP</span>
             <span>TECH</span>
             <span>PROJ</span>
@@ -28,8 +28,13 @@ function App() {
           </div>
         </div>
       </header>
-      Hello World!
-      <p className="text-3xl">This is my portfolio</p>
+      <section className="w-[70vw] h-screen" id="summary">
+        <div className="flex items-center justify-between text-xs font-space text-charcoal-faint py-10">
+          <span>37°46'N / 122°25'W</span>
+          <span>NODE.ID :: RA-0042 // SECTOR.7</span>
+          <span>INIT.SEQ :: 0x1F4A</span>
+        </div>
+      </section>
     </div>
   );
 }
