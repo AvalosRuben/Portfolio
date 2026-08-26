c8fb8db7f23fd4de52fa0fba7c73ec6073f1c496
AvalosRuben <rubenavalosmtz@gmail.com>
2026-08-25 19:29:20 -0600

    feat(Projects): Added missing margin top to the project component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 function Projects() {
   return (
-    <section className="w-[70vw] h-screen" id="projects">
+    <section className="w-[70vw] h-screen mt-4" id="projects">
       <div className="flex items-center gap-3 ">
         <div className="h-2 w-2 bg-charcoal shrink-0" />
         <span className="font-courier text-sm text-charcoal font-bold tracking-wider shrink-0">
