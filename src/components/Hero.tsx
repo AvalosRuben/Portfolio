048134a203f6cd44a03eb5073aaa0685c93b19b9
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-10 17:08:51 -0600

    feat(Hero): Took the TopBar out of the hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,33 +4,6 @@ import InfoTag from "./ui/InfoTag";
 function Hero() {
   return (
     <div className="w-full h-fit pb-10 flex flex-col items-center">
-      <header className="border-b border-charcoal/35 bg-cream-dark sticky top-0 z-50 w-full font-space tracking-wider">
-        <div className="border-charcoal/10  border-b py-4 px-6 flex items-center justify-between text-xs text-charcoal-light ">
-          <span>SYS.CLOCK :: 19.8.2026 // UTC-6</span>{" "}
-          {/*TODO - Set to the actual date*/}
-          <div className="flex gap-10 items-center">
-            <span>SYS.ONLINE</span>
-            <span>P(FILT) 45Pa</span>
-            <span>VFD 19%</span>
-            <span className="text-charcoal font-bold">CELLS • 19%</span>
-            <span>PORT......3</span>
-          </div>
-        </div>
-        <div className="flex items-center justify-between py-3 text-sm px-6 font-bold text-charcoal">
-          <div className="flex gap-2 items-center justify-between">
-            {/*TODO - Add a figure */}
-            <span>RUBEN AVALOS // PORTFOLIO v1.0</span>
-          </div>
-          <div className="flex gap-10 items-center">
-            {/*TODO - Change to nav and buttons to set the scroll to id*/}
-            <span>SUMMARY</span>
-            <span>EXP</span>
-            <span>TECH</span>
-            <span>PROJ</span>
-            <span>CONNECT</span>
-          </div>
-        </div>
-      </header>
       <section className="w-[70vw] h-fit" id="summary">
         <div className="flex items-center justify-between text-xs font-space text-charcoal-faint py-10">
           <span>37°46'N / 122°25'W</span>
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,33 @@
+function TopBar() {
+  return (
+    <header className="border-b border-charcoal/35 bg-cream-dark sticky top-0 z-50 w-full font-space tracking-wider">
+      <div className="border-charcoal/10  border-b py-4 px-6 flex items-center justify-between text-xs text-charcoal-light ">
+        <span>SYS.CLOCK :: 19.8.2026 // UTC-6</span>{" "}
+        {/*TODO - Set to the actual date*/}
+        <div className="flex gap-10 items-center">
+          <span>SYS.ONLINE</span>
+          <span>P(FILT) 45Pa</span>
+          <span>VFD 19%</span>
+          <span className="text-charcoal font-bold">CELLS • 19%</span>
+          <span>PORT......3</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-between py-3 text-sm px-6 font-bold text-charcoal">
+        <div className="flex gap-2 items-center justify-between">
+          {/*TODO - Add a figure */}
+          <span>RUBEN AVALOS // PORTFOLIO v1.0</span>
+        </div>
+        <div className="flex gap-10 items-center">
+          {/*TODO - Change to nav and buttons to set the scroll to id*/}
+          <span>SUMMARY</span>
+          <span>EXP</span>
+          <span>TECH</span>
+          <span>PROJ</span>
+          <span>CONNECT</span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default TopBar;
