925f5d0e90c1d5efba7a3fc88d00d0c3bb3f72c0
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-10 17:28:04 -0600

    feat(TopBar): Added the TopBar to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <div className="flex flex-col  bg-cream micro-grid text-charcoal items-center">
+      <TopBar />
       <Hero />
       <Projects />
     </div>
