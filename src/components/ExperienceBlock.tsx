6065c289f45ea6f41d74eab17250d32b533d4902
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-12 16:44:44 -0600

    feat(Experience): Experiencie Block component created with the needed props

---
+++ src/components/ExperienceBlock.tsx
@@ -0,0 +1,21 @@
+interface ExperiencieBlockProps {
+  Title: string;
+  Company: string;
+  Place: string;
+  StartDate: string;
+  FinishDate: string;
+  Descriptions: string[];
+}
+
+function ExperiencieBlock({
+  Title,
+  Company,
+  Place,
+  StartDate,
+  FinishDate,
+  Descriptions,
+}: ExperiencieBlockProps) {
+  return null;
+}
+
+export default ExperiencieBlock;
