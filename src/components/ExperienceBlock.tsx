b2c9d5de5de159a3880ac5436d004afce7c8ca9e
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-12 17:57:26 -0600

    feat(Experience): Added changes to the font sizes depending on the screen size

--- src/components/ExperienceBlock.tsx
+++ src/components/ExperienceBlock.tsx
@@ -24,18 +24,18 @@ function ExperiencieBlock({
       </span>
       <div className="w-full flex flex-col gap-2 justify-center ">
         <div className="flex items-center justify-between">
-          <span className="text-charcoal font-bold font-space tracking-wide">
+          <span className="text-charcoal font-bold text-sm md:text-base font-space tracking-wide">
             {Title.toUpperCase()}
           </span>
-          <span className="text-charcoal-faint font-space text-xs">
+          <span className="text-charcoal-faint font-space text-xxs md:text-sm">
             {StartDate} - {FinishDate}
           </span>
         </div>
-        <span className="mr-auto text-charcoal-faint font-space text-xs">
+        <span className="mr-auto text-charcoal-faint font-space text-xxs md:text-sm">
           {Company.toUpperCase()} - {Place.toUpperCase()}
         </span>
         <div className="border-b pb-2 border-charcoal-faint/40">
-          <div className="flex flex-col font-space text-sm gap-2 border-dashed border-t border-charcoal-faint/30 my-2 pt-2">
+          <div className="flex flex-col font-space text-xs md:text-sm gap-2 border-dashed border-t border-charcoal-faint/30 my-2 pt-2">
             {Descriptions.map((d) => (
               <span>▸ {d}</span>
             ))}
